881c77e87177f2ec2c00af1dcd2f22af0b107aca
Federico <13087870+s0rc3r3r01@users.noreply.github.com>
2026-02-08 17:52:52 +0000

    stuff

--- httpgordle/internal/handlers/newgame/handler.go
+++ httpgordle/internal/handlers/newgame/handler.go
@@ -3,5 +3,10 @@ package newgame
 import "net/http"
 
 func Handle(w http.ResponseWriter, req *http.Request) {
-	_, _ = w.Write([]byte("test"))
+	if req.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	_, _ = w.Write([]byte("Creating new game"))
 }
--- httpgordle/internal/handlers/router.go
+++ httpgordle/internal/handlers/router.go
@@ -8,6 +8,6 @@ import (
 
 func Mux() *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.HandleFunc(api.NewGameRoute, newgame.Handle)
+	mux.HandleFunc(http.MethodPost+" "+api.NewGameRoute, newgame.Handle)
 	return mux
 }
