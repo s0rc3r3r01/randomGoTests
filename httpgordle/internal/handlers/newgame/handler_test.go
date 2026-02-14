39d8f2d7882cbe67170359390da557889953158a
Federico <13087870+s0rc3r3r01@users.noreply.github.com>
2026-02-14 18:15:57 +0000

    add initial tests for newgame handler and update go.mod and go.sum with required dependencies

---
+++ httpgordle/internal/handlers/newgame/handler_test.go
@@ -0,0 +1,21 @@
+package newgame
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHandl(t *testing.T) {
+	req, err := http.NewRequest(
+		http.MethodPost, "/games", nil)
+	require.NoError(t, err)
+	recorder := httptest.NewRecorder()
+	Handle(recorder, req)
+	assert.Equal(t, http.StatusCreated, recorder.Code)
+	assert.Equal(t, "application/json", recorder.Header().Get("Content-Type"))
+	assert.JSONEq(t, `{"id":"","attempts_left":0,"guesses":null,"solution":"","wordlength":0,"status":""}`, recorder.Body.String())
+}
