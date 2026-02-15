51b15479550d38015753758a4860216d385e331f
Federico <13087870+s0rc3r3r01@users.noreply.github.com>
2026-02-15 10:51:48 +0000

    ok

--- httpgordle/internal/handlers/guess/handler.go
+++ httpgordle/internal/handlers/guess/handler.go
@@ -3,16 +3,30 @@ package guess
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 	"randomGoTests/httpgordle/internal/api"
 )
 
 func Handle(w http.ResponseWriter, req *http.Request) {
 	r := api.GuessRequest{}
+	id := req.PathValue(api.GameID)
+	if id == "" {
+		http.Error(w, "missing id", http.StatusBadRequest)
+		return
+	}
 	err := json.NewDecoder(req.Body).Decode(&r)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-
+	apiGame := api.GameResponse{
+		ID: id,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(apiGame)
+	if err != nil {
+		// The header has already been set. Nothing much we can do here.
+		log.Printf("failed to write response: %s", err)
+	}
 }
--- httpgordle/internal/handlers/guess/handler_test.go
+++ httpgordle/internal/handlers/guess/handler_test.go
@@ -1,1 +1,25 @@
 package guess
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"randomGoTests/httpgordle/internal/api"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHandle(t *testing.T) {
+	body := strings.NewReader(`{"guess":"pocket"}`)
+	req, err := http.NewRequest(http.MethodPost, "/games/", body)
+	require.NoError(t, err)
+	req.SetPathValue(api.GameID, "123456")
+
+	recorder := httptest.NewRecorder()
+	Handle(recorder, req)
+	assert.Equal(t, http.StatusOK, recorder.Code)
+	assert.Equal(t, "application/json", recorder.Header().Get("Content-Type"))
+	assert.JSONEq(t, `{"id":"123456","attempts_left":0,"guesses":null,"solution":"","wordlength":0,"status":""}`, recorder.Body.String())
+}
